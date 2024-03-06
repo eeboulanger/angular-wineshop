54598f47415b4652f9dae522644bc438bfb49640
Liza <eboulanger.pro@gmail.com>
2024-03-06 12:25:51 +0100

    added dynamic update of categories in side menu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,13 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = "http://localhost:8080/api/products"
+  private categoryUrl = "http://localhost:8080/api/product-category";
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> {
 
-    //SHOW ALL PRODUCTS
+    //If CATEGORY ID IS ZERO THEN SHOW ALL PRODUCTS
     if (categoryId == 0) {
       return this.httpClient.get<Product[]>(this.baseUrl).pipe(map(response => response));
     }
@@ -23,7 +25,10 @@ export class ProductService {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
     return this.httpClient.get<GetResponse>(searchUrl).pipe(map(response => response.content));
+  }
 
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<ProductCategory[]>(this.categoryUrl).pipe(map(response => response));
   }
 }
 
